281dfe5e5096a36f74d8a6b38547cb1df3909dd1
Thorsten Hacke <thorsten_hacke@web.de>
2020-05-02 12:20:41 +0200

    Moved AutoCompletionHandler to new File and optimized

---
+++ ConsoleApp1/AutoCompletionHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class AutoCompletionHandler : IAutoCompleteHandler
+    {
+        private const char Separator = '\\';
+
+        // characters to start completion from
+        public char[] Separators { get; set; } = new char[] {  };
+
+        // text - The current text entered in the console
+        // index - The index of the terminal cursor within {text}
+        public string[] GetSuggestions(string text, int index)
+        {
+            if (text.Length == 0)
+            {
+                return Environment.GetLogicalDrives();
+            }
+            if (!text.Contains(Separator))
+            {
+                var drives = Environment.GetLogicalDrives().Where(ld => ld.StartsWith(text, StringComparison.OrdinalIgnoreCase));
+                return drives.ToArray();
+            }
+            var parts = text.Split(Separator);
+            var knownPart = text.Substring(0, text.LastIndexOf(Separator))+ Separator;
+            var entries = Directory.EnumerateFileSystemEntries(knownPart);
+            var newPart = parts.Last();
+            var found = entries.Where(e => IsPathMatch(e, knownPart, newPart));
+            return found.ToArray();
+        }
+
+        private bool IsPathMatch(string path, string knownPart, string newPart) {
+            var cleanedPath = path.Replace(knownPart, string.Empty);
+            return cleanedPath.StartsWith(newPart);
+        }
+    }
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,5 @@
 ﻿using PowerPointGenerator;
 using System;
-using System.IO;
-using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -25,32 +23,6 @@ namespace ConsoleApp1
             generator.SaveInDestination(savePath);
             Console.WriteLine("Presentation saved. Press any key to close");
             Console.ReadKey();
-        }
-    }
-
-    class AutoCompletionHandler : IAutoCompleteHandler
-    {
-        // characters to start completion from
-        public char[] Separators { get; set; } = new char[] {  };
-
-        // text - The current text entered in the console
-        // index - The index of the terminal cursor within {text}
-        public string[] GetSuggestions(string text, int index)
-        {
-            if (text.Length == 0)
-            {
-                return Environment.GetLogicalDrives();
-            }
-            var parts = text.Split('\\');
-            var drives = Environment.GetLogicalDrives().Where(ld => ld.StartsWith(parts[0], StringComparison.OrdinalIgnoreCase));
-            if (!text.Contains('\\'))
-            {
-                return drives.ToArray();
-            }
-            var knownPart = text.Substring(0, text.LastIndexOf('\\'))+"\\";
-            var entries = Directory.EnumerateFileSystemEntries(knownPart);
-            var found = entries.Where(e => e.Replace(knownPart,"").StartsWith(parts[parts.Length - 1]));
-            return found.ToArray();
         }
     }
 
